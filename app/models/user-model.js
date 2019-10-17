ead0d74e024dd450c9ec4d4b6bfda799eb784998
Christian Fries <hallo@christian-fries.ch>
2019-10-17 17:32:44 +0200

    Don't require name properties

--- app/models/user-model.js
+++ app/models/user-model.js
@@ -13,11 +13,9 @@ const UserSchema = new mongoose.Schema(
         },
         firstName: {
             type: String,
-            required: true
         },
         lastName: {
             type: String,
-            required: true
         }
     }, {
         timestamps: true
