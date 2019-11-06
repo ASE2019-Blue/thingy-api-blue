380257c89281afdb79001c2df58687d90348c06c
Christian <jacktraror@users.noreply.github.com>
2019-11-06 23:20:17 +0100

    Add the demo game for sprint 1

--- app/controllers/demo-controller.js
+++ app/controllers/demo-controller.js
@@ -1,4 +1,25 @@
 const Mqtt = require('../mqtt');
+const Utilities = require('../services/utility-service');
+
+/**
+ * Starts the demo process
+ *
+ * @param ctx
+ * @param next
+ * @returns {Promise<void>}
+ */
+async function demo(ctx, next) {
+    Mqtt.client.subscribe('e3:af:9b:f4:a1:c7/Thingy User Interface Service/Thingy Button Characteristic');
+    Mqtt.client.on('message', async function (topic, message) {
+        // console.log(message.toString());
+        if(message=='true'){
+            noColour();
+            await Utilities.sleep(1500);
+            changeColor();
+        }
+    });
+    changeColor();
+}
 
 /**
  * Change Thingy LED to random color.
@@ -7,7 +28,7 @@ const Mqtt = require('../mqtt');
  * @param next
  * @returns {Promise<void>}
  */
-async function demo(ctx, next) {
+async function changeColor() {
     const colors = [
         '1,255,0,0',
         '1,0,255,0',
@@ -19,9 +40,32 @@ async function demo(ctx, next) {
     Mqtt.client.publish(
         'e3:af:9b:f4:a1:c7/Thingy User Interface Service/Thingy LED Characteristic/Set',
         color);
-    ctx.body = color;
+}
+
+/**
+ * Sets the colour of the LED to (0,0,0) so OFF
+ */
+async function noColour() {
+    Mqtt.client.publish(
+        'e3:af:9b:f4:a1:c7/Thingy User Interface Service/Thingy LED Characteristic/Set',
+        '1,0,0,0');
+}
+
+/**
+ * Stops the demo (unsubsribes from button characteristics)
+ *
+ * @param ctx
+ * @param next
+ * @returns {Promise<void>}
+ */
+async function stop(ctx, next) {
+    Mqtt.client.unsubscribe('e3:af:9b:f4:a1:c7/Thingy User Interface Service/Thingy Button Characteristic');
+    Mqtt.client.publish(
+        'e3:af:9b:f4:a1:c7/Thingy User Interface Service/Thingy LED Characteristic/Set',
+        '1,3,1,0');
 }
 
 module.exports = {
-    demo
+    demo,
+    stop
 };
--- app/models/game-model.js
+++ app/models/game-model.js
@@ -2,7 +2,8 @@
 
 const TAP_GAME = 'tap-game';
 const HIDE_AND_SEEK = 'hide-and-seek';
-const GAME_KEYS = [TAP_GAME, HIDE_AND_SEEK];
+const DEMO = 'demo';
+const GAME_KEYS = [TAP_GAME, HIDE_AND_SEEK, DEMO];
 
 const GAMES = [
     {
@@ -17,8 +18,14 @@ const GAMES = [
         description: 'Description of the game «Hide and seek»',
         icon: 'game'
     },
+    {
+        key: DEMO,
+        title: 'Demo 1',
+        description: 'This is a demo for Sprint 1',
+        icon: 'game'
+    },
 ];
 
 module.exports = {
-    TAP_GAME, HIDE_AND_SEEK, GAME_KEYS, GAMES
+    TAP_GAME, HIDE_AND_SEEK, DEMO, GAME_KEYS, GAMES
 };
