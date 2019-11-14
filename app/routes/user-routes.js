e5d2f9b5a08ed00af164adcd7bc815e34d05cff6
Christian Fries <hallo@christian-fries.ch>
2019-11-14 11:09:15 +0100

    Add basic travis configuration

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -6,7 +6,7 @@ const Ctrl = require('../controllers/user-controller');
 
 router.get('/', Ctrl.findAll)
     .get('/:username', Ctrl.findOne)
-    .put('/:username',Ctrl.change)
+    .put('/:username', Ctrl.change)
     .put('/:username/password', Ctrl.changePassword);
 
 module.exports = router.routes();
