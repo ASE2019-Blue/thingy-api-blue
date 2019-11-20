212379e8f2e55685dec9fbbacd751c3f3936e821
Christian <jacktraror@users.noreply.github.com>
2019-11-20 18:37:51 +0100

    Extend user model with favorite thingy TG-38 #closed

--- app/routes/user-routes.js
+++ app/routes/user-routes.js
@@ -7,6 +7,7 @@ const Ctrl = require('../controllers/user-controller');
 router.get('/', Ctrl.findAll)
     .get('/:username', Ctrl.findOne)
     .put('/:username', Ctrl.change)
-    .put('/:username/password', Ctrl.changePassword);
+    .put('/:username/password', Ctrl.changePassword)
+    .put('/:username/thingy',Ctrl.changeFavoriteThingy);
 
 module.exports = router.routes();
