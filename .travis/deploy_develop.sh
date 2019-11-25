7cd195d8570017ec19bb880c4ed0d7b78897460a
Christian Fries <hallo@christian-fries.ch>
2019-11-25 11:21:03 +0100

    Add debug output to deployment script

--- .travis/deploy_develop.sh
+++ .travis/deploy_develop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+ls -la ../
+ls -la ./
+ls -la .travis/
+
 eval "$(ssh-agent -s)"
-chmod 600 .travis/id_rsa
-ssh-add .travis/id_rsa
+chmod 600 ./.travis/id_rsa
+ssh-add ./.travis/id_rsa
 
 ssh -o StrictHostKeyChecking=no root@51.15.250.22 ./deploy_develop.sh
