ecda37cd25706ed42c0b54776870d4db8aaa7255
Christian Fries <hallo@christian-fries.ch>
2019-11-28 19:21:38 +0100

    Implement endpoint to get high score values
    TG-126 #closed

---
+++ app/controllers/highscore-controller.js
@@ -0,0 +1,29 @@
+const Game = require('../models/game-model');
+const HighScore = require('../models/highscore-model');
+
+/**
+ * Find the best players for each game.
+ *
+ * @param ctx
+ * @param next
+ * @returns {Promise<void>}
+ */
+async function findBest(ctx, next) {
+    // // Create test data
+    // const highScore = new HighScore();
+    // highScore.gameKey = Game.TAP_GAME;
+    // highScore.match = '5dd9813633803a55dd05d478';
+    // highScore.user = 'tester';
+    // highScore.score = 96;
+    // await highScore.save();
+
+    const highScores = {};
+    highScores[Game.TAP_GAME] = await HighScore.find({ gameKey: Game.TAP_GAME }).sort({ score: -1 }).limit(5);
+    highScores[Game.HIDE_AND_SEEK] = await HighScore.find({ gameKey: Game.HIDE_AND_SEEK }).sort({ score: -1 }).limit(5);
+
+    ctx.body = highScores;
+}
+
+module.exports = {
+    findBest,
+};
