c6324c81d6834c5917ffb3ec82ded1798387f64f
Christian <jacktraror@users.noreply.github.com>
2019-12-12 10:27:59 +0100

    Change test to using server

--- test/controllers/game-controller.test.js
+++ test/controllers/game-controller.test.js
@@ -3,14 +3,43 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const expect = require('chai').expect;
 
-const app = require('../mock-server');
-const server = app;
+// const app = require('../mock-server');
+// const server = app;
+// require('dotenv').config();
+// const server = require('../../app/index');
+const userCredentials = {
+  username: 'chai', 
+  password: 'chai'
+}
 
 describe("Route: games/", () => {
+  let token;
+  before(() => {
+    chai
+        .request('http://localhost:3000')
+        .post('/auth/token')
+        .set('content-type', 'application/json')
+        .send(userCredentials)
+        .end((err, res) => {
+          expect(err).to.not.exist;
+          expect(res.status).to.be.eql(200);
+          expect(res.type).to.be.eql("application/json");
+          expect(res.body).to.satisfy(function (val) {
+            console.log(res.body);
+            token = res.body;
+            return true;
+          });
+        });
+  });
+
+  after(() => {
+
+  });
+
   describe("Sub-route: GET /", () => {
     it("Should return list of games", done => {
       chai
-        .request(server)
+        .request('http://localhost:3000')
         .get('/games')
         .end((err, res) => {
           expect(err).to.not.exist;
@@ -28,7 +57,7 @@ describe("Route: games/", () => {
     it("With correct gameKey: should return the rating of the game", done => {
       var gameKey = "hide-and-seek";
       chai
-        .request(server)
+        .request('http://localhost:3000')
         .get('/games/'+gameKey+'/rating')
         .end((err, res) => {
           expect(err).to.not.exist;
@@ -43,7 +72,7 @@ describe("Route: games/", () => {
     it("With wrong gameKey: should return 404, Game not found", done => {
       var gameKey = "BLOB";
       chai
-        .request(server)
+        .request('http://localhost:3000')
         .get('/games/'+gameKey+'/rating')
         .end((err, res) => {
           expect(res.status).to.be.eql(404);
@@ -56,12 +85,12 @@ describe("Route: games/", () => {
   // describe("Sub-route: PUT /:gameKey/rating", () => {
   //   it("With correct gameKey: should return the rating of the game", done => {
   //     var gameKey = "hide-and-seek";
-  //     chai
-  //       .request(server)
+  //     let tmp = chai
+  //       .request('http://localhost:3000')
   //       .post('/games/'+gameKey+'/rating')
   //       .set('content-type', 'application/json')
-  //       .send({rating: 3})
-  //       .end((err, res) => {
+  //       .send({rating: 3});
+  //       tmp.end((err, res) => {
   //         expect(res.status).to.be.eql(200);
   //         done();
   //       });
@@ -69,7 +98,7 @@ describe("Route: games/", () => {
   //   it("With wrong gameKey: should return 404, Game not found", done => {
   //     var gameKey = "BLOB";
   //     chai
-  //       .request(server)
+  //       .request('http://localhost:3000')
   //       .post('/games/'+gameKey+'/rating')
   //       .set('content-type', 'application/json')
   //       .send({rating: 3})
@@ -83,7 +112,7 @@ describe("Route: games/", () => {
   describe("Sub-route: GET /colors", () => {
     it("Should return list of games", done => {
       chai
-        .request(server)
+        .request('http://localhost:3000')
         .get('/games/colors')
         .end((err, res) => {
           expect(err).to.not.exist;
