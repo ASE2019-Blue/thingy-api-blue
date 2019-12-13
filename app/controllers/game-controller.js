dcab7aade0b495260175cd4a496960fb4707aaf5
Christian <jacktraror@users.noreply.github.com>
2019-12-13 13:04:55 +0100

    Lint changes

--- app/controllers/game-controller.js
+++ app/controllers/game-controller.js
@@ -142,7 +142,7 @@ async function getRating(ctx, next) {
     if (!Game.GAME_KEYS.includes(gameKey)) {
         ctx.throw(404, { error: 'Game not found' });
     }
-    
+
     ctx.body = await calculateRating(gameKey);
 }
 
